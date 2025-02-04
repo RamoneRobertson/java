25a6313e88f2d86d3f3ddced9bbc3077449ac1bf
Ramone Robertson <ramonerobertson10@gmail.com>
2025-02-04 21:11:27 +0900

    finished cars assemble challenge

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,26 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        double productionRate = 0.0;
+        if(speed < 5){
+            productionRate = speed * 221;
+        }
+        else if(speed <= 8){
+            productionRate = speed * (221 * 0.9);
+        }
+        else if(speed == 9){
+            productionRate = speed * (221 * 0.8);
+        }
+        else{
+            productionRate = speed * (221 * 0.77);
+        }
+
+        return productionRate;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        double perHourRate = productionRatePerHour(speed);
+        double perMinuteRate = perHourRate / 60;
+        return (int) perMinuteRate;
     }
 }
